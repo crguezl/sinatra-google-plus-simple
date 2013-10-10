afd69917e352d0c2ab1e7f2dd8980d621dea97ec
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-10-10 19:01:17 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,29 @@
+How do I use Sinatra to access the Google+ API?
+==============================================
+
+Code based in the article by SATISH TALIM on SEPTEMBER 20, 2011
+-------------------------------------
+
+[Article in http://rubylearning.com](http://rubylearning.com/blog/2011/09/20/how-do-i-use-sinatra-to-access-the-google-api/)
+
+This an attempt to follow the aforementioned article by Satish Salim.
+
+[Code available at gist.github.com](https://gist.github.com/SatishTalim/1228369#file_index.erb)
+
+
+See also
+--------
+
+* The [google_plus gem](http://rubydoc.info/gems/google_plus/) documentation
+
+* alainravet [ Google+ Ruby Starter Project](https://github.com/alainravet/google-plus-ruby-starter)
+in Github
+
+* To access the Google+ API, get your own Google API key at 
+[https://code.google.com/apis/console/?pli=1#access](https://code.google.com/apis/console/?pli=1#access)
+
+* Note down the Google+ ID of the person whose Google+ profile you
+want to display using this app. For example, here’s my Google+ URL
+and the number in the URL namely 117566254824665007664 is my Google+
+ID.
+
---
+++ sinatragplus.rb
@@ -0,0 +1,50 @@
+# sinatragplus.rb
+require 'sinatra'
+require "sinatra/config_file"
+require 'google_plus'
+
+config_file 'config.yml'
+
+
+error do
+  erb :'500'
+end
+
+#class
+class GPlus
+  def initialize(apikey, gid)
+    @apikey = apikey
+    @gid = gid
+    get_info
+  end
+
+  attr_reader :row0, :row1, :row2, :logo
+  private
+    #Get info about specific G+ ID
+    def get_info
+      GooglePlus.api_key = 'AIzaSyC5FFIdxj8ZoBHf3KnUU8clfWBmASc0ZOA'
+      begin
+        GooglePlus.api_key = @apikey
+        person = GooglePlus::Person.get(@gid.to_i)
+        @row0 = person.display_name
+        @row1 = person.list_activities.items
+        @row2 = person.url
+        properties = person.attributes
+        puts properties.class
+        puts properties['image'].class
+        @logo = properties['image']['url']
+        properties.each { |key, value| puts "%-20s %20s" % [key, value] }
+      rescue Exception => msg  
+        # display the system generated error message  
+        puts msg  
+      end  
+    end
+end
+ 
+# Display Google+ details
+get '/' do
+  @key = settings.key
+  @gplus = GPlus.new(@key, '117566254824665007664')
+  erb :show
+end
+
